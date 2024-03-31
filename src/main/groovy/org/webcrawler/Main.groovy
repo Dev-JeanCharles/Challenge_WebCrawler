87f955e7389b8433b3fe716526069d7fb33e752a
Dev-JeanCharles <jeancduasan@gmail.com>
2024-03-31 02:29:36 -0300

    feat: Atualizando o Main

--- src/main/groovy/org/webcrawler/Main.groovy
+++ src/main/groovy/org/webcrawler/Main.groovy
@@ -4,5 +4,7 @@ import org.webcrawler.Operations.Operation
 
 static void main(String[] args) {
     Operation operation = new Operation()
-
+    operation.buscarTabela()
+    operation.buscarHistorico()
+    operation.buscarTabelaErros()
 }
